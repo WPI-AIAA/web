5a83dfb107f9b35fdb17d4bd747e480b6bcc80df
Robaire <galliath.robaire@gmail.com>
2017-12-11 02:34:41 -0500

    Added NotFound.vue to catch 404 errors

---
+++ src/components/NotFound.vue
@@ -0,0 +1,19 @@
+<template>
+
+	<div>
+
+		<h1> Oops! It seems the page you are looking for does not exist. </h1>
+	</div>
+
+</template>
+
+<script>
+
+	export default {
+		name: 'NotFound'
+	}
+
+</script>
+
+<style>
+</style>
