d758d104969a9483731144a27bc4e8a4368c5be0
Robaire <galliath.robaire@gmail.com>
2017-12-10 23:33:40 -0500

    Changed <a> to <router-link>

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -2,44 +2,25 @@
 
 	<div id="navigationBar">
 		
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
+		  <router-link class="navbar-brand" to="/"> WPI-AIAA </router-link>
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
 
+		  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+		    <div class="navbar-nav">
+		      <router-link class="nav-item nav-link" to="/">Home</router-link>
+		      <router-link class="nav-item nav-link" to="/about">About</router-link>
+		      <router-link class="nav-item nav-link" to="/resources">Resources</router-link>
+		      <router-link class="nav-item nav-link" to="/login">Login</router-link>
+		    </div>
+		  </div>
 
+		</nav>
 
+		<div id="buffer"/>
 
 	</div>
 
@@ -56,5 +37,12 @@
 
 <style scope>
 
+	#buffer {
+		margin: 30px;
+	}
+
+	nav {
+		background-color: #222222;
+	}
 
 </style>
