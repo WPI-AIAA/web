22d2b824249b8028119e3cc5e7f79b6a1db7c272
Robaire <galliath.robaire@gmail.com>
2018-01-11 13:14:52 -0500

    Cosmetic changes

--- src/components/Resources.vue
+++ src/components/Resources.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
 		<h1> Calendar </h1>
-		<iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=wpi.aiaa%40gmail.com&amp;color=%231B887A&amp;ctz=America%2FNew_York" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+		<iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=wpi.aiaa%40gmail.com&amp;color=%231B887A&amp;ctz=America%2FNew_York" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
 	</div>
 </template>
 
@@ -14,4 +14,8 @@
 </script>
 
 <style scope>
+
+	iframe{
+		width: 60%;
+	}
 </style>
